8f80bf765b64619bba1a34ad402a092b2f074085
dancozmuta <Dan@Dan-2.fritz.box>
2022-04-18 10:47:12 +0200

    Project created and styling defined

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './navbar.scss'
+
+class Navbar extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <nav>Navbar</nav>
+        );
+    }
+}
+ 
+export default Navbar;
---
+++ src/components/product.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Product extends Component {
+    state = { 
+
+     } 
+    render() { 
+        return (
+            <div>Product Title</div>
+        );
+    }
+}
+ 
+export default Product;
